b234e0e8749fcbe2f971a23d8a5dc5bc952fe591
AzizbekAzimjonovich <azizbekazimjonovich2003@gmail.com>
2024-04-02 20:15:14 +0500

    vazifa

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+import TripList from './components/TripList'
+
+function App() {
+
+  return (
+    <>
+     <TripList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TripList.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import {useFetch} from '../hooks/useFetch'
+
+import './TripList.css'
+
+function TripList() {
+
+    //const [trip, setTrip] = useState([])
+
+    const [url, setUrl] = useState('http://localhost:3000/trips')
+    const {data: trip, isPending}=useFetch(url)
+   /** const FTrip = useCallback(async () => {
+        const req = await fetch(url)
+        const data = await req.json()
+        setTrip(data)
+    }, [url])
+
+    useEffect(() => {
+        FTrip()
+    }, [FTrip]) */
+
+
+    return (
+        <div className="trip-list">
+            <h1>Trip List</h1>
+            { isPending && <div><p>loading..</p></div> }
+            <ul>
+                {trip && trip.map((trip) => {
+                    return (
+                        <li key={trip.title}>
+                            <p>{trip.title}</p>
+                            <p>{trip.price}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div className="filters">
+                <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>Europe</button>
+                <button onClick={() => setUrl('http://localhost:3000/trips')}>All</button>
+            </div>
+        </div>
+    )
+}
+
+export default TripList
